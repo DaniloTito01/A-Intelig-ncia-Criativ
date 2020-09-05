3ae49d5d587445c4358ba76eed1233a8badc7a23
DaniloTito01 <36375486+DaniloTito01@users.noreply.github.com>
2020-09-04 23:25:37 -0300

    por

--- app.js
+++ app.js
@@ -1,157 +1,159 @@
-// var express = require('express')
+var express = require('express')
 
-// const app = express();
-// const cors = require('cors')
+const app = express();
+const cors = require('cors')
 
-// //require('dotenv/config');
+//require('dotenv/config');
 
-// app.use(cors())
-
-
-// //  const banco =  require('./db');
-// //  const Cliente = require('./db');
-
-// var data = new Date();
-// var dia     = data.getDate();           // 1-31
-// var mes     = 1 +data.getMonth();          // 0-11 (zero=janeiro) 
-// var ano4    = data.getFullYear();  
-// var hora    = data.getHours();          // 0-23
-// var min     = data.getMinutes();   
+app.use(cors())
 
 
+//  const banco =  require('./db');
+//  const Cliente = require('./db');
 
-// var dadoss = 
-//     {
-//         nome: '',
-//         email: '',
-//         telefone: '',
-//         estado: '',
-//         cidade: '',
-//         whatsapp: '',
-//         emailbox: ''
-// }
+var data = new Date();
+var dia     = data.getDate();           // 1-31
+var mes     = 1 +data.getMonth();          // 0-11 (zero=janeiro) 
+var ano4    = data.getFullYear();  
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();   
 
 
-// const  nome= ''
+
+var dadoss = 
+    {
+        nome: '',
+        email: '',
+        telefone: '',
+        estado: '',
+        cidade: '',
+        whatsapp: '',
+        emailbox: ''
+}
+
+
+const  nome= ''
        
-//         // email: '',
-//         // telefone: '',
-//         // estado: '',
-//         // cidade: '',
-//         // whatsapp: '',
-//         // emailbox: ''
+        // email: '',
+        // telefone: '',
+        // estado: '',
+        // cidade: '',
+        // whatsapp: '',
+        // emailbox: ''
 
 
 
 
-// app.use(express.json());
+app.use(express.json());
 
 
 
-// app.get("/tes", (req, res) =>{
-//     res.send("Alo  Mundo!! ");
+app.get("/tes", (req, res) =>{
+    res.send("Alo  Mundo!! ");
 
    
-// });
+});
 
-// app.post("/ps", (req, res ) =>{
+app.post("/ps", (req, res ) =>{
    
   
-//  res.send("OK");
+ res.send("OK");
 
 
 
 
 
-//  dadoss =  req.body;
+ dadoss =  req.body;
 
-//  name = dadoss.dados.nome;
-//  email1 = dadoss.dados.email;
-//  console.log(name);
+ name = dadoss.dados.nome;
+ email1 = dadoss.dados.email;
+ console.log(name);
 
 
-//  if(!name==''){
-//     console.log("Passou " +  dadoss.dados.email + dadoss.dados.nome  );
+ if(!name==''){
+    console.log("Passou " +  dadoss.dados.email + dadoss.dados.nome  );
 
-//     // Cliente.create({
+    // Cliente.create({
   
   
-//     //   nome:dadoss.dados.nome,
-//     //   email:dadoss.dados.email,
-//     //   telefone:dadoss.dados.telefone,
-//     //   estado:dadoss.dados.estado,      
-//     //   cidade:dadoss.dados.cidade,
-//     //   emailbox:dadoss.dados.emailbox,
-//     //   whatsbox:dadoss.dados.whatsappBox
+    //   nome:dadoss.dados.nome,
+    //   email:dadoss.dados.email,
+    //   telefone:dadoss.dados.telefone,
+    //   estado:dadoss.dados.estado,      
+    //   cidade:dadoss.dados.cidade,
+    //   emailbox:dadoss.dados.emailbox,
+    //   whatsbox:dadoss.dados.whatsappBox
     
     
-//     //  })
+    //  })
    
-//     // banco.gravarBanco(dadoss.dados.nome,dadoss.dados.email,dadoss.dados.telefone,dadoss.dados.estado,dadoss.dados.cidade,dadoss.dados.whatsappBox,dadoss.dados.emailbox);
+    // banco.gravarBanco(dadoss.dados.nome,dadoss.dados.email,dadoss.dados.telefone,dadoss.dados.estado,dadoss.dados.cidade,dadoss.dados.whatsappBox,dadoss.dados.emailbox);
 
  
-//    // email(dadoss.dados.nome,dadoss.dados.email,dadoss.dados.telefone,dadoss.dados.estado,dadoss.dados.cidade,dadoss.dados.whatsappBox,dadoss.dados.emailbox);
+   // email(dadoss.dados.nome,dadoss.dados.email,dadoss.dados.telefone,dadoss.dados.estado,dadoss.dados.cidade,dadoss.dados.whatsappBox,dadoss.dados.emailbox);
 
-//  }
-//  console.log(dadoss + "DATA" + dia + "/0" + mes +"/" + ano4  +"Horas" + hora +":" + min);
-
-
-// });
+ }
+ console.log(dadoss + "DATA" + dia + "/0" + mes +"/" + ano4  +"Horas" + hora +":" + min);
 
 
+});
 
-// app.listen(process.env.PORT || 3000,() => {
-//     console.log("API Start");
 
+
+app.listen((process.env.PORT || 5000), function(){
+    console.log('listening on *:5000');
+    console.log("API Start");
+  });
+    
+
+
+
+
+
+
+// function email(nome,email1,telefone,estado,cidade,whatsappBox,emailbox){
+
+// //dadosEmail.nome =dadoss.dados.nome;
+
+//      // 0-59
+
+
+//     const nodemailer = require('nodemailer');
+    
+//     let transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: process.env.NODE_USER,   
+//             pass: process.env.NODE_PASS        
+//         }
+//     });
+    
+
+   
+// transporter.sendMail({
+//     from: '"A Inteligência Criativa" + process.env.NODE_USER', // sender address
+//     to:process.env.NODE_TO ,// list of receivers
+//     subject:'Lead: '+ email1, // Subject line
+//     text: "Teste", //'Data:13/08/2020 Nome :  Danilo  , E-mail: danilo.tito@hotmail.com , Telefone: (11)  7070-7070 ,Estado: São Paulo, Cidade : São Paulo  ', // plain text body
+//     html: '<h1>Lead </h1>'   
+//     +'<br> <b>Nome: </b>'   + nome     
+//     +'<br> <b>Email: </b>' + email1          
+//     +'<br> <b>Telefone: </b>' + telefone          
+//     +'<br> <b>Estado: </b>' + estado          
+//     +'<br> <b>Cidade: </b>'+ cidade       
+//     +'<br> <b>Receber por Whatsapp: </b>' + whatsappBox           
+//     +'<br> <b>Receber por Email: </b>' + emailbox      
+//     +'<br> <b>Data:  </b>' + dia + "/0" + mes +"/" + ano4      
+//     +'<br> <b>Hora: </b>'  + hora +":" +min     
+
+// }).then(messge =>{
+//   console.log(messge);
+// }).catch(err =>{
+// console.log(err)
 // })
 
 
-
-
-
-// // function email(nome,email1,telefone,estado,cidade,whatsappBox,emailbox){
-
-// // //dadosEmail.nome =dadoss.dados.nome;
-
-// //      // 0-59
-
-
-// //     const nodemailer = require('nodemailer');
-    
-// //     let transporter = nodemailer.createTransport({
-// //         host: 'smtp.gmail.com',
-// //         port: 465,
-// //         secure: true,
-// //         auth: {
-// //             user: process.env.NODE_USER,   
-// //             pass: process.env.NODE_PASS        
-// //         }
-// //     });
-    
-
-   
-// // transporter.sendMail({
-// //     from: '"A Inteligência Criativa" + process.env.NODE_USER', // sender address
-// //     to:process.env.NODE_TO ,// list of receivers
-// //     subject:'Lead: '+ email1, // Subject line
-// //     text: "Teste", //'Data:13/08/2020 Nome :  Danilo  , E-mail: danilo.tito@hotmail.com , Telefone: (11)  7070-7070 ,Estado: São Paulo, Cidade : São Paulo  ', // plain text body
-// //     html: '<h1>Lead </h1>'   
-// //     +'<br> <b>Nome: </b>'   + nome     
-// //     +'<br> <b>Email: </b>' + email1          
-// //     +'<br> <b>Telefone: </b>' + telefone          
-// //     +'<br> <b>Estado: </b>' + estado          
-// //     +'<br> <b>Cidade: </b>'+ cidade       
-// //     +'<br> <b>Receber por Whatsapp: </b>' + whatsappBox           
-// //     +'<br> <b>Receber por Email: </b>' + emailbox      
-// //     +'<br> <b>Data:  </b>' + dia + "/0" + mes +"/" + ano4      
-// //     +'<br> <b>Hora: </b>'  + hora +":" +min     
-
-// // }).then(messge =>{
-// //   console.log(messge);
-// // }).catch(err =>{
-// // console.log(err)
-// // })
-
-
-// // }
+// }
 
